fc040b8003dbe9354fe81adc33e0c0e2056d92c0
TGusciora <tgusciora@gmail.com>
2021-09-09 17:54:37 +0200

    Update 09/09/2021
    *dodanie opisu danych
    *data cleaning

--- TG281998_01_Data_Input.R
+++ TG281998_01_Data_Input.R
@@ -40,7 +40,7 @@ cc_data <- read.csv("data/UCI_Credit_Card.csv")
 
 table(cc_data$SEX)
 table(cc_data$EDUCATION)
-table(cc_data$MARRIAGE)//
+table(cc_data$MARRIAGE)
 
 sapply(cc_data, 
        function(x) sum(x == ""))
@@ -56,11 +56,7 @@ summary(cc_data)
 table(cc_data$default.payment.next.month)
 
 
-#zamiana zmiennej celu i zmiennych kategorycznych na factory
-cc_data=cc_data %>% mutate(default.payment.next.month=as.factor(default.payment.next.month))
-cc_data=cc_data %>% mutate(SEX=as.factor(SEX))
-cc_data=cc_data %>% mutate(EDUCATION=as.factor(EDUCATION))
-cc_data=cc_data %>% mutate(MARRIAGE=as.factor(MARRIAGE))
+#CZYSZCZENIE DANYCH 
 
 table(cc_data$SEX)
 table(cc_data$EDUCATION)
@@ -70,28 +66,46 @@ table(cc_data$MARRIAGE)
 #The 0 in MARRIAGE can be safely categorized as 'Other' (thus 3).
 #The 0 (undocumented), 5 and 6 (label unknown) in EDUCATION can also be put in a 'Other' cathegory (thus 4)
 
+#cc_data <- data.frame(cc_data)
+#PROBLEM - jak zamienic wartosc 0 na 3? 2021-09-08
+cc_data$MARRIAGE[cc_data$MARRIAGE==0] <- 3 #zmiana wartosci 0 na wartosc 3 - inne
+cc_data$EDUCATION[cc_data$EDUCATION==0] <- 4 #zmiana wartosci 0 na wartosc 3 - inne
+cc_data$EDUCATION[cc_data$EDUCATION==5] <- 4 #zmiana wartosci 5 na wartosc 3 - inne
+cc_data$EDUCATION[cc_data$EDUCATION==6] <- 4 #zmiana wartosci 6 na wartosc 3 - inne
 
-
+table(cc_data$EDUCATION)
 table(cc_data$MARRIAGE)
 
-cc_data <- data.frame(cc_data)
-#PROBLEM - jak zamienic wartosc 0 na 3? 2021-09-08
-cc_data[cc_data$MARRIAGE==0] <- 3
 
-df[df==1]<-11
+#zamiana zmiennej celu i zmiennych kategorycznych na factory
+cc_data=cc_data %>% mutate(default.payment.next.month=as.factor(default.payment.next.month))
+cc_data=cc_data %>% mutate(SEX=as.factor(SEX))
+cc_data=cc_data %>% mutate(EDUCATION=as.factor(EDUCATION))
+cc_data=cc_data %>% mutate(MARRIAGE=as.factor(MARRIAGE))
+
 
 levels(cc_data$SEX) <- c("Mężczyzna", "Kobieta")
 levels(cc_data$EDUCATION) <- c("Magister lub wyższe","Licencjat lub równoważne","Szkola średnia","Inne")
-levels(cc_data$MARRIAGE) <- c("Brak danych","Zamężna/żonaty","Kawaler/panna","Inne")
+levels(cc_data$MARRIAGE) <- c("Zamężna/żonaty","Kawaler/panna","Inne")
 
+table(cc_data$SEX)
+table(cc_data$EDUCATION)
 table(cc_data$MARRIAGE)
 
 
-barplot(table(cc_data$default.payment.next.month), 
-        col = "gold", 
-        horiz = TRUE,  # wykres horyzontalny
-        main = "Rozkład zmiennej zależnej w próbie - brak zapłacenia raty", # tytuł
-        xlab = "liczebność") # etykieta osi x
+
+#spróbuj napisac petle na nazwie zmiennej
+### NARAZIE NIE WIEM JAK
+table(cc_data$PAY_0)
+
+for (year in c(0,2,3,4,5,6)){
+        paste('cc_data$PAY_', year,'[cc_data$PAY_',year,'==-1] <- 0')
+        paste('cc_data$PAY_', year,'[cc_data$PAY_',year,'==-1] <- 0')
+}
+
+
+table(cc_data$PAY_0)
+table(cc_data$PAY_6)
 
 
 
@@ -105,27 +119,4 @@ barplot(table(cc_data$default.payment.next.month),
 
 
 
-hist(cc_data$default.payment.next.month,
-     main = "Histogram tygodniowej liczby godzin pracy",
-     xlab = "liczba godzin pracy",
-     ylab = "liczebność",
-     col = "light green")
-
-#scatter ma sens dla ciaglych, dla factor dac tabele czestosci?
-plot(cc_data$SEX, # liczba godzin pracy w tygodniu
-     cc_data$default.payment.next.month,  # wynagrodzenie z pracy w tysiącach
-     xlab = "Cena biletu w $",
-     ylab = "Wiek")
-
-
-#PO CO MI DATA FRAME I FACTORY? 
-#ZMIEN FONT NA POLSKI Z OBSŁUGĄ POLSKICH ZNAKÓW - UTF8
-
-cc_data$sex_factor <- as.factor(cc_data$sex)
-cc_data$education_factor <- as.factor(cc_data$education)
-cc_data$marriage_factor <- as.factor(cc_data$marriage)
-
-cc_data$sex_factor = factor(cc_data$sex, levels=c("1","2"), labels=c("Mezczyzna","Kobieta"))
-cc_data$education = factor(cc_data$education, levels=c("1","2","3","4"), labels=c("Magister lub wyzsze","Licencjat lub rownowazne","Szkola srednia","Inne"))
-cc_data$marriage = factor(cc_data$marriage, levels=c("1","2"), labels=c("zamezna/zonaty","kawaler/panna","inne"))
 
